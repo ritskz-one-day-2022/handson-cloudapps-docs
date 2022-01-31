05773d6930622fb390c9af184c8177c22f3daa1a
asash3 <9412135+asash3@users.noreply.github.com>
2022-02-01 03:14:51 +0900

    ランディングページ追加

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,10 @@
+import { navbar, sidebar } from './configs'
+
+module.exports = {
+  lang: 'ja-JP',
+  title: 'Webアプリ開発ハンズオン',
+  description: 'クラウドベースのWebアプリケーション開発体験',
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,9 @@
-# Hello VuePress
+---
+home: true
+title: Home
+actions:
+  - text: はじめに
+    link: /handson/
+    type: primary
+footer: MIT Licensed | Copyright © 2022-present asash3
+---
