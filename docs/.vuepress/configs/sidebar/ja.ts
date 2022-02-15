a60cf352ff25e6c97f4cb8b7f6f8d478a9336c77
asash3 <9412135+asash3@users.noreply.github.com>
2022-02-15 15:10:23 +0900

    [add] ナビゲーションバーに目次追加

--- docs/.vuepress/configs/sidebar/ja.ts
+++ docs/.vuepress/configs/sidebar/ja.ts
@@ -6,6 +6,26 @@ export const ja: SidebarConfig = {
       text: 'ハンズオン',
       children: [
         '/handson/README.md',
+        {
+          text: 'STEP 1: WEB アプリの開発準備をする',
+          link: '/handson/module-one.html'
+        },
+        {
+          text: 'STEP 2: REACT アプリをホストする',
+          link: '/handson/module-two.html'
+        },
+        {
+          text: 'STEP 3: 認証を追加する',
+          link: '/handson/module-three.html'
+        },
+        {
+          text: 'STEP 4: API とデータベースを追加する',
+          link: '/handson/module-four.html'
+        },
+        {
+          text: 'STEP 5: ストレージを追加する',
+          link: '/handson/module-five.html'
+        },
       ],
     },
   ]
